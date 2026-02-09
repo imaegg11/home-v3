d2c17db2381cad5d43f65275e0b0d2840320d3c5
imaegg11 <95944377+imaegg11@users.noreply.github.com>
2026-02-08 21:10:06 -0500

    hello world

--- src/entry-server.jsx
+++ src/entry-server.jsx
@@ -6,9 +6,10 @@ export default createHandler(() => (
     document={({ assets, children, scripts }) => (
       <html lang="en">
         <head>
+          <title>Homepage</title>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
+          {/* <link rel="icon" href="/favicon.ico" /> */}
           {assets}
         </head>
         <body>
